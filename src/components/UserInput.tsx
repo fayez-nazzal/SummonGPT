b55c24c7a0058924972bb64683cf7e0796142010
Fayez Nazzal <soulldev@gmail.com>
2023-03-11 19:46:35 +0200

    Allow submitting UserInput

--- src/components/UserInput.tsx
+++ src/components/UserInput.tsx
@@ -4,40 +4,49 @@ import TextareaAutosize from "solid-textarea-autosize";
 
 export interface ITextInputProps {
   class?: string;
-  [key: string]: any;
+  onSubmit?: (value: string) => void;
 }
 
- const UserInput = (props: ITextInputProps) => {
-   let inputRef: HTMLTextAreaElement | undefined = undefined;
+const UserInput = (props: ITextInputProps) => {
+  let inputRef: HTMLTextAreaElement | undefined = undefined;
 
-   createEffect(() => {
-     inputRef?.focus();
-   });
+  createEffect(() => {
+    inputRef?.focus();
+  });
 
-   const onblur: JSX.EventHandler<HTMLTextAreaElement, FocusEvent> = (e) => {
-     e.preventDefault();
-     inputRef?.focus();
-   };
+  const onblur: JSX.EventHandler<HTMLTextAreaElement, FocusEvent> = (e) => {
+    e.preventDefault();
+    inputRef?.focus();
+  };
 
-   return (
-     <TextareaAutosize
-       ref={inputRef}
-       {...props}
-       placeholder="Write your message here, press enter to send."
-       class={scn(
-         "bg-plane text-textPrimary placeholder:textPrimary/80",
-         "text-md",
-         "resize-none",
-         "border border-melt/20",
-         "rounded-lg cursor-text p-4 focus:outline-none ",
-         props.class
-       )}
-       autofocus
-       onblur={onblur}
-       minRows={1}
-       maxRows={10}
-     />
-   );
- };
+  const onkeydown: JSX.EventHandler<HTMLTextAreaElement, KeyboardEvent> = (
+    e
+  ) => {
+    if (e.key === "Enter" && e.currentTarget.value) {
+      e.preventDefault();
+      props.onSubmit?.(e.currentTarget.value);
+      e.currentTarget.value = "";
+    }
+  };
+
+  return (
+    <TextareaAutosize
+      ref={inputRef}
+      placeholder="Write your message here, press Enter to send."
+      class={scn(
+        "bg-plane text-textPrimary placeholder:textPrimary/80",
+        "text-md",
+        "resize-none",
+        "border border-melt/20",
+        "rounded-lg cursor-text p-4 focus:outline-none ",
+        props.class
+      )}
+      autofocus
+      onblur={onblur}
+      minRows={1}
+      maxRows={10}
+    />
+  );
+};
 
  export default UserInput;

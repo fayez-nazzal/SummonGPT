45c374cd451bd35589b54e8cc342b8ed52d1ae50
Fayez Nazzal <soulldev@gmail.com>
2023-03-11 04:15:32 +0200

    init tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div class="container"></div>;
+  return <div class="w-screen h-screen">hello</div>;
 }
 
 export default App;
--- src/components/Header.tsx
+++
@@ -1,7 +0,0 @@
-export const Header = () => {
-  return (
-    <header>
-      <h1>SummonGPT</h1>
-    </header>
-  );
-};
